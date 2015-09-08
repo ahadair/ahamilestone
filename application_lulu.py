302324c12497da6b7038e88d176803c062a26912
Antony <ahadair@MacBookPro1.local>
2015-09-08 06:31:53 -0400

    AHAMilestoneV0

--- application_lulu.py
+++ application_lulu.py
@@ -25,6 +25,7 @@ print "Declarations"
 
 @app_lulu.route('/index_lulu',methods=['GET','POST'])
 def index_lulu():
+    print "Index Lulu"
     nquestions=app_lulu.nquestions
     if request.method == 'GET':
         return render_template('userinfo_lulu.html',num=nquestions)
@@ -34,16 +35,17 @@ def index_lulu():
         #        app_lulu.vars['age'] = request.form['age_lulu']
         app_lulu.vars['ticker'] = request.form['ticker_lulu']
 
+        #print "CPrice Value =", request.form['cprice_lulu']
+        #app_lulu.vars['cprice'] = request.form['cprice_lulu']
+
+        app_lulu.vars['cprice'] = request.form['cprice_lulu']
+        app_lulu.vars['aprice'] = request.form['aprice_lulu']
+        app_lulu.vars['volume'] = request.form['volume_lulu']
 
         print "Ticker Value =", app_lulu.vars['ticker']
-
-        #print "CPrice Value =", app_lulu.vars['cprice_lulu']
-
-        #app_lulu.vars['cprice'] = request.form['cprice_lulu']
-        #        app_lulu.vars['aprice'] = request.form['aprice_lulu']
-        #        app_lulu.vars['volume'] = request.form['volume_lulu']
-
-
+        print "CPrice =", app_lulu.vars['cprice']
+        print "APrice =", app_lulu.vars['aprice']
+        print "Volume =", app_lulu.vars['volume']
 
 
         #f = open('%s_%s_%s.txt'%(app_lulu.vars['name'],app_lulu.vars['age'], app_lulu.vars['ticker']),'w')
@@ -67,46 +69,53 @@ def index_lulu():
 
 @app_lulu.route('/main_lulu')
 def main_lulu2():
-    if len(app_lulu.questions)==0 : return render_template('end_lulu.html')
-    return redirect('/next_lulu')
+    print "Main lulu2"
+    return render_template('end_lulu.html')
 
 
-
-#####################################
-## IMPORTANT: I have separated /next_lulu INTO GET AND POST
-## You can also do this in one function, with If and Else.
-
-@app_lulu.route('/next_lulu',methods=['GET'])
-def next_lulu():  #remember the function name does not need to match the URL
-    # for clarity (temp variables):
-    n=app_lulu.nquestions-len(app_lulu.questions)+1
-    q=app_lulu.questions.keys()[0] #python indexes at 0
-    a1=app_lulu.questions[q][0]
-    a2=app_lulu.questions[q][1]
-    a3=app_lulu.questions[q][2]
-
-    # save current question
-    app_lulu.currentq=q
-
-    return render_template('layout_lulu.html',num=n,question=q,ans1=a1,ans2=a2,ans3=a3)
+#    if len(app_lulu.questions)==0 : return render_template('end_lulu.html')
+#    return redirect('/next_lulu')
 
 
-
-@app_lulu.route('/next_lulu',methods=['POST'])
-def next_lulu2():  #can't have two functions with the same name
-    # Here, we will collect data from the user.
-    # Then, we return to the main function, so it can tell us whether to 
-    # display another question page, or to show the end page.
-
-    #f=open('%s_%s_%s.txt'%(app_lulu.vars['name'],app_lulu.vars['age'],app_lulu.vars['ticker']),'a') #a is for append
-    f=open('%s.txt'%(app_lulu.vars['ticker']),'a') #a is for append
-    f.write('%s\n'%(app_lulu.currentq))
-    f.write('%s\n\n'%(request.form['answer_from_layout_lulu'])) #do you know where answer_lulu comes from?
-    f.close()
-
-    app_lulu.questions.pop(app_lulu.currentq)
-
-    return redirect('/main_lulu')
+#
+######################################
+### IMPORTANT: I have separated /next_lulu INTO GET AND POST
+### You can also do this in one function, with If and Else.
+#
+#@app_lulu.route('/next_lulu',methods=['GET'])
+#def next_lulu():  #remember the function name does not need to match the URL
+#    print "Next Lulu"
+#    # for clarity (temp variables):
+#    n=app_lulu.nquestions-len(app_lulu.questions)+1
+#    q=app_lulu.questions.keys()[0] #python indexes at 0
+#    a1=app_lulu.questions[q][0]
+#    a2=app_lulu.questions[q][1]
+#    a3=app_lulu.questions[q][2]
+#
+#    # save current question
+#    app_lulu.currentq=q
+#
+#    return render_template('layout_lulu.html',num=n,question=q,ans1=a1,ans2=a2,ans3=a3)
+#
+#
+#
+#@app_lulu.route('/next_lulu',methods=['POST'])
+#def next_lulu2():  #can't have two functions with the same name
+#    # Here, we will collect data from the user.
+#    # Then, we return to the main function, so it can tell us whether to 
+#    # display another question page, or to show the end page.
+#    print "Next Lulu2"
+#
+#
+#    #f=open('%s_%s_%s.txt'%(app_lulu.vars['name'],app_lulu.vars['age'],app_lulu.vars['ticker']),'a') #a is for append
+#    f=open('%s.txt'%(app_lulu.vars['ticker']),'a') #a is for append
+#    f.write('%s\n'%(app_lulu.currentq))
+#    f.write('%s\n\n'%(request.form['answer_from_layout_lulu'])) #do you know where answer_lulu comes from?
+#    f.close()
+#
+#    app_lulu.questions.pop(app_lulu.currentq)
+#
+#    return redirect('/main_lulu')
 
 if __name__ == "__main__":
     app_lulu.run()
