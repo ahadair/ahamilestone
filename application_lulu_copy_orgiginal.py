df95ecd6a6ea66f1b612a8972b5d7c11d125f782
Antony <ahadair@MacBookPro1.local>
2015-09-07 22:27:07 -0400

    Nothing to Say

--- application_lulu_copy_orgiginal.py
+++ application_lulu_copy_orgiginal.py
@@ -4,24 +4,12 @@ app_lulu = Flask(__name__)
 app_lulu.vars={}
 
 app_lulu.questions={}
-#app_lulu.questions['How many eyes do you have?']=('1','2','3')
-
-
-#app_lulu.getclosingprice = False
-#app_lulu.getadjustedclosingprice = False
-#app_lulu.getvolume = False
-
-
-
-##app_lulu.questions['Which fruit do you like best?']=('banana','mango','pineapple')
-##app_lulu.questions['Do you like cupcakes?']=('yes','no','maybe')
+app_lulu.questions['How many eyes do you have?']=('1','2','3')
+app_lulu.questions['Which fruit do you like best?']=('banana','mango','pineapple')
+app_lulu.questions['Do you like cupcakes?']=('yes','no','maybe')
 
 app_lulu.nquestions=len(app_lulu.questions)
 #should be 3
-
-print "Declarations"
-
-
 
 @app_lulu.route('/index_lulu',methods=['GET','POST'])
 def index_lulu():
@@ -30,47 +18,20 @@ def index_lulu():
         return render_template('userinfo_lulu.html',num=nquestions)
     else:
         #request was a POST
-        #        app_lulu.vars['name'] = request.form['name_lulu']
-        #        app_lulu.vars['age'] = request.form['age_lulu']
-        app_lulu.vars['ticker'] = request.form['ticker_lulu']
+        app_lulu.vars['name'] = request.form['name_lulu']
+        app_lulu.vars['age'] = request.form['age_lulu']
 
-
-        print "Ticker Value =", app_lulu.vars['ticker']
-
-        #print "CPrice Value =", app_lulu.vars['cprice_lulu']
-
-        #app_lulu.vars['cprice'] = request.form['cprice_lulu']
-        #        app_lulu.vars['aprice'] = request.form['aprice_lulu']
-        #        app_lulu.vars['volume'] = request.form['volume_lulu']
-
-
-
-
-        #f = open('%s_%s_%s.txt'%(app_lulu.vars['name'],app_lulu.vars['age'], app_lulu.vars['ticker']),'w')
-        f = open('%s.txt'%(app_lulu.vars['ticker']),'w')
-        #f = open('%s%s%s%s.txt'% (app_lulu.vars['ticker'],app_lulu.vars['cprice'],app_lulu.vars['aprice'],app_lulu.vars['volume'],'w')
-
-
-        #f.write('Name: %s\n'%(app_lulu.vars['name']))
-        #f.write('Age: %s\n\n'%(app_lulu.vars['age']))
-        f.write('Ticker: %s\n\n'%(app_lulu.vars['ticker']))
-
-        #f.write('Closing Price: %s\n\n'%(app_lulu.vars['cprice']))
-        #f.write('Adjusted Closing Price: %s\n\n'%(app_lulu.vars['aprice']))
-        #f.write('Volume: %s\n\n'%(app_lulu.vars['volume']))
-
+        f = open('%s_%s.txt'%(app_lulu.vars['name'],app_lulu.vars['age']),'w')
+        f.write('Name: %s\n'%(app_lulu.vars['name']))
+        f.write('Age: %s\n\n'%(app_lulu.vars['age']))
         f.close()
 
         return redirect('/main_lulu')
-
-
 
 @app_lulu.route('/main_lulu')
 def main_lulu2():
     if len(app_lulu.questions)==0 : return render_template('end_lulu.html')
     return redirect('/next_lulu')
-
-
 
 #####################################
 ## IMPORTANT: I have separated /next_lulu INTO GET AND POST
@@ -90,16 +51,13 @@ def next_lulu():  #remember the function name does not need to match the URL
 
     return render_template('layout_lulu.html',num=n,question=q,ans1=a1,ans2=a2,ans3=a3)
 
-
-
 @app_lulu.route('/next_lulu',methods=['POST'])
 def next_lulu2():  #can't have two functions with the same name
     # Here, we will collect data from the user.
     # Then, we return to the main function, so it can tell us whether to 
     # display another question page, or to show the end page.
 
-    #f=open('%s_%s_%s.txt'%(app_lulu.vars['name'],app_lulu.vars['age'],app_lulu.vars['ticker']),'a') #a is for append
-    f=open('%s.txt'%(app_lulu.vars['ticker']),'a') #a is for append
+    f=open('%s_%s.txt'%(app_lulu.vars['name'],app_lulu.vars['age']),'a') #a is for append
     f.write('%s\n'%(app_lulu.currentq))
     f.write('%s\n\n'%(request.form['answer_from_layout_lulu'])) #do you know where answer_lulu comes from?
     f.close()
